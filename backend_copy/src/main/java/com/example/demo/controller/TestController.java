e173e2abcf570ac50be8471cbd6e56b8e3c70fb7
ssrforever <ljx.arrebol@gmail.com>
2026-05-15 16:20:29 +0800

    delete copy dir

--- backend_copy/src/main/java/com/example/demo/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-@RequestMapping("/api")
-@CrossOrigin // 允许跨域，否则前端连不上后端
-public class TestController {
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "来自 SpringBoot 后端的非遗平台数据连接成功！";
-    }
-}
