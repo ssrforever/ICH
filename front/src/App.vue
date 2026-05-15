ec589554c646990f21be21f3fd7ffae6fc8f4343
ssrforever <ljx.arrebol@gmail.com>
2026-05-15 15:02:33 +0800

    feat: 完成前后端初始化及跨域连接测试

---
+++ front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>非遗文化推广平台</h1>
+    <p>后端状态：{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return { message: '正在连接后端...' }
+  },
+  mounted() {
+    axios.get('http://localhost:8080/api/hello')
+      .then(res => { this.message = res.data })
+      .catch(err => { this.message = '连接失败：' + err })
+  }
+}
+</script>
